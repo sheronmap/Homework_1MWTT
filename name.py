9c0c4ed5a407b7716ad48af6f47a1ed43ac6120c
sheronmap <sheronmap@gmail.com>
2018-07-19 22:22:22 +0200

    Initial commit

---
+++ name.py
@@ -0,0 +1,9 @@
+# name
+
+first_name = input("What's your first name? ")
+
+middle_name = input("What's your middle name? ")
+
+last_name = input("What's your last name? ")
+
+print("Hi "+first_name+" "+middle_name+" "+last_name + "!" + " How are you today?")
