46791396c316ff52bb871ab45757f276c9653f50
sheronmap <sheronmap@gmail.com>
2018-07-20 14:00:47 +0200

    Completed favourite_number

--- favourite_number.py
+++ favourite_number.py
@@ -1,10 +1,16 @@
 # favourite number
 
-favourite_number = input("What is your favourite number?")
+favourite_number = input("What is your favourite number? ")
 
-#if favourite_number.isdigit():
-if type(eval(favourite_number)) == int:
-	new_favourite_number = int(favourite_number) + 1
-	print("Your favourite number sucks! Try "+str(new_favourite_number)+" for a favourite!")
-else:
-	print("You have not entered a number, please enter a number. ")
+try:
+	if type(eval(favourite_number)) ==int:
+		new_favourite_number = int(favourite_number) + 1
+		print("Your favourite number sucks! Try "+str(new_favourite_number)+" , it's a bigger and better number!")
+	elif type(eval(favourite_number)) ==float:
+		new_favourite_number = float(favourite_number) + 1
+		print("Your favourite number sucks! Try "+str(new_favourite_number)+" , it's a bigger and better number!")
+	else:
+		print("You have not entered a number, please enter a number. ")
+except: print("You have not entered a number, please enter a number. ")		
+
+		
